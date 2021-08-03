3ca73d444e12863b90a33cbb6384af57003b5596
deanellerby <dean.ellerby@gmail.com>
2021-08-03 22:24:00 +0100

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -66,17 +66,7 @@ $OOBEDeployJson = @'
 }
 '@
 $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
-#=======================================================================
-#   PostOS: OOBEDeploy CMD File
-#=======================================================================
-$AutopilotCmd = @'
-PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
-set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
-start PowerShell -NoL -W Mi
-start /wait PowerShell -NoL -C Install-Module OSD -Force
-start /wait PowerShell -NoL -C Start-OOBEDeploy
-'@
-$AutopilotCmd | Out-File -FilePath "C:\Windows\Autopilot.cmd" -Encoding ascii -Force
+
 #=======================================================================
 #   PostOS: Restart-Computer
 #=======================================================================
